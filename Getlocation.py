89b6f1a08264e0c30823e769a20ab3dcb00e24e6
jiayaw99 <jiayaw1999@hotmail.com>
2020-05-26 01:13:16 +0800

    beautiful

--- Getlocation.py
+++ Getlocation.py
@@ -1,27 +1,18 @@
-import codecs
-import urllib
+
 import webbrowser
-from random import random
 
 import googlemaps
-import xml.etree.ElementTree as et
-
-
-import urllib3 as urllib3
-from Tools.scripts import google
-from bs4 import BeautifulSoup
 from gmplot import gmplot
-
-import plotly.offline as ply
 import os
-
 path = 'options'
-colors = ['red','blue','green','purple','orange','yellow','pink','white']
+colors = ['red','blue','gray','purple','fuchsia','gold','pink','white']
 files = os.listdir(path)
+# center of map
 gmap = gmplot.GoogleMapPlotter(4.144082, 101.081438, 8)
+gmap.apikey='AIzaSyB-T4FGL-A6zZ_p6lbNgD4eHtjnV9b44a0'
 
 for (ind,f) in enumerate (files,1):
-	print("Option ",ind," = ",f.split('.')[0])
+	print("Option ",ind," (",colors[ind-1],") = ",f.split('.')[0])
 
 strr = ""
 count=0;
@@ -39,32 +30,8 @@ for ls in files:
         coordinatey.append((float)(temp[0]))
         coordinatex.append((float)(temp[1]))
 
-    gmap.plot(coordinatex, coordinatey, colors[count], edge_width=5)
+    gmap.plot(coordinatex, coordinatey, colors[count], edge_width=6)
     count+=1
-
-# UM TO USM
-# option 1 = taxi to klia, flight to penang airport,
-# center of map
-
-
-
-# input array of coordinates from kml files
-
-
-# Scatter points
-# top_attraction_lats, top_attraction_lons = zip(*[
-#     (37.769901, -122.498331),
-#     (37.768645, -122.475328),
-#     (37.771478, -122.468677),
-#     (37.769867, -122.466102),
-#     (37.767187, -122.467496),
-#     (37.770104, -122.470436)
-#     ])
-# gmap.scatter(top_attraction_lats, top_attraction_lons, '#3B0B39', size=40, marker=False)
-
-# Marker
-# hidden_gem_lat, hidden_gem_lon = 37.770776, -122.461689
-# gmap.marker(hidden_gem_lat, hidden_gem_lon, 'cornflowerblue')
 
 # Draw
 gmap.draw("my_map.html")
