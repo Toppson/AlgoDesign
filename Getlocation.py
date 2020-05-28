d3190b28a17a4721be85e11139600fd112e2778d
jiayaw99 <jiayaw1999@hotmail.com>
2020-05-29 01:04:23 +0800

    distance

--- Getlocation.py
+++ Getlocation.py
@@ -11,6 +11,7 @@ files = os.listdir(path)
 gmap = gmplot.GoogleMapPlotter(4.144082, 101.081438, 8)
 gmap.apikey='AIzaSyB-T4FGL-A6zZ_p6lbNgD4eHtjnV9b44a0'
 
+
 for (ind,f) in enumerate (files,1):
 	print("Option ",ind," (",colors[ind-1],") = ",f.split('.')[0])
 
@@ -21,7 +22,8 @@ for ls in files:
     data = open("options/" + ls, 'r').read().replace("\n", "")
     strr = ' '.join(data.split())
     strr = strr.replace(" ", "&")
-
+    fullcoordinatex = []
+    fullcoordinatey = []
     coor = strr.split("&")
     coordinatex = []
     coordinatey = []
@@ -29,7 +31,8 @@ for ls in files:
         temp = i.split(',')
         coordinatey.append((float)(temp[0]))
         coordinatex.append((float)(temp[1]))
-
+    fullcoordinatex.append(coordinatex)
+    fullcoordinatey.append(coordinatey)
     gmap.plot(coordinatex, coordinatey, colors[count], edge_width=6)
     count+=1
 
@@ -37,3 +40,11 @@ for ls in files:
 gmap.draw("my_map.html")
 url = "my_map.html"
 webbrowser.open(url)
+distance=[]
+for i in len(fullcoordinatex):
+    for k in len(fullcoordinatex[i])-1:
+        #temp= getdistance(fullcoordinatex[i][k],fullcoordinatey[i][k]) between (fullcoordinatex[i][k+1],fullcoordinatey[i][k+1])
+        temp+=temp
+    distance.append(temp)
+
+#//find shortest distance and show graph
