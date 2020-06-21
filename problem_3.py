55b430d970a20d08dd80521979b90e21d8f0e098
gootacatchitall <50090044+gootacatchitall@users.noreply.github.com>
2020-06-21 22:10:39 +0800

    Update problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -1,94 +1,94 @@
-from SemanticAnalysis import positiveword,negativeword
-from Getlocation import distance,files
+from Semantic Analysis import positiveword,negativeword
+from Getlocation import distance
+
+#read option pf path
+with open('options.txt') as myfile:
+    line = myfile.read().split("\n")
 
 
 
 
-# distances= ranking of path according to distance in Shortest Path
-distances = [0] * 7
+preference = [0] *( len(line)-1)
 
-# the first element of distances =7 and the last =1
-for i in range(7):
-    distances[i] = i
-
-preference = [0] *( len(files)-1)
-
-car = 10
-flight = 10
-bus = 10
-ktm = 10
-lrt_mrt = 10
-grab = 10
-taxi = 10
-ferry = 10
-walk = 5
+transport=[10]*7
+transport_string=['bus','ferry','lrt and mrt','ktm','grab','taxi','flight']
 
 # calculate level of preference for each transportation, if negative then tranportation-=1
 for i in range(3):
     if positiveword[i]<negativeword[i]:
-        bus-=1
-
+        transport[0]-=1
     if positiveword[i+3]<negativeword[i+3]:
-        ferry-=1
+        transport[1]-=1
     if positiveword[i+6]<negativeword[i+6]:
-        lrt_mrt-=1
+        transport[2]-=1
 
     if positiveword[i+9]<negativeword[i+9]:
-        ktm-=1
+        transport[3]-=1
 
     if positiveword[i+12] < negativeword[i+12]:
-        grab -= 1
+        transport[4]-=1
 
     if positiveword[i+15] < negativeword[i + 15]:
-        taxi-= 1
+        transport[5]-=1
     if positiveword[i+18] < negativeword[i + 18]:
-        flight -= 1
+        transport[6]-=1
+
+for i in range(len(transport)):
+    if transport[i]==7:
+        print(transport_string[i],'shows negative sentiment')
+    else:
+        print(transport_string[i],'shows positive sentiment')
+
+
+
 
 
 #calculate total preference of transportation used in each option
-for (ind,f) in enumerate (files,1):
-    sub_option=f.split(',')
-    for x in range(len(sub_option)):
-        if str(sub_option[j]).__contains__('car'):
-            temp += car
-        elif str(sub_option[j]).__contains__('ferry'):
-            temp += ferry
-        elif str(sub_option[j]).__contains__('bus'):
-            temp += bus
-        elif str(sub_option[j]).__contains__('flight'):
-            temp += flight
-        elif (str(sub_option[j]).__contains__('komuter') or str(sub_option[j]).__contains__('ets')):
-            temp += ktm
-        elif str(sub_option[j]).__contains__('lrt') or str(sub_option[j]).__contains__('ets'):
-            temp += lrt_mrt
-        elif str(sub_option[j]).__contains__('grab'):
-            temp += grab
-        elif str(sub_option[j]).__contains__('taxi'):
-            temp += taxi
-        elif str(sub_option[j]).__contains__('walk'):
-            temp += walk
+for i in range(len(line)-1):
+    path = line[i].split(',')
+    temp = 0
+    for j in range(len(path)):
+        if path[j].__contains__('bus'):
+            temp += transport[0]
+
+        elif path[j].__contains__('ferry'):
+            temp += transport[1]
+        elif path[j].__contains__('lrt') or path[j].__contains__('mrt'):
+            temp += transport[2]
+
+        elif (path[j].__contains__('komuter') or path[j].__contains__('ets')):
+            temp += transport[3]
+
+        elif path[j].__contains__('grab'):
+            temp +=transport[4]
+        elif path[j].__contains__('taxi'):
+            temp += transport[5]
+        elif path[j].__contains__('flight'):
+            temp += transport[6]
+        elif path[j].__contains__('walk'):
+            temp += 5
     preference[i] = temp / len(path)
 
 
-#distance = distance of path from Getlocation,sort option accendingly according to distance
-for i in range(len(distances)):
-    for j in range(0, len(distances) - 1 - i):
-        if distance[j] < distance[j + 1]:
-            distances[j],distances[j + 1] =distances[j + 1],distances[j]
-            distance[j], distance[j + 1] = distance[j + 1], distance[j]
-            preference[j], preference[j + 1] = preference[j + 1], preference[j]
-            files[j], files[j + 1] = files[j + 1], files[j]
+#index=rank of path according to distance,shorter distance=lowest mark
+index=[0]*len(distance)
+for i, x in enumerate(sorted(range(len(distance)), key=lambda y: distance[y])):
+    index[x] = i
 
-# add distances to preference, preference=ranking of path according to distance and preference of article
-for i in range(len(distances)):
-    preference[i]+=distances[i]
+#
+for i in range(7):
+    preference[i]+=index[i]
 
 
+max_value=preference[0]
 max=0
+for i in range (len(preference)-1):
 
-for i in range (len(distances)-1):
+    if(preference[i]<max_value):
+        max=i
+        max_value=preference[i]
 
-    if(preference[i]<preference[i+1]):
-        max=i+1
 
-print('the best option is taking',files[max])
+
+
+print('the best option is taking',line[max])
