032d053918888f5e4c449af1fe8862ccd826b497
Toppson <wid180020@siswa.um.edu.my>
2020-06-23 00:59:14 +0800

    rubbish cleared

--- Information_extraction.py
+++ Information_extraction.py
@@ -32,14 +32,12 @@ links.append("https://www.freemalaysiatoday.com/category/nation/2020/06/11/malay
 links.append("https://www.thestar.com.my/news/nation/2020/06/08/malaysia-airlines-lifting-travel-restrictions-and-reopening-borders-will-allow-loved-ones-to-reunite")#flight2
 links.append("https://www.malaysiaairports.com.my/media-centre/news/latest-updates-new-routes") #flight3
 
-#
-# myurl=urllib.request.urlopen("http://tuxworld.wordpress.com")
+
 url_links=[]
 for i in range(len(links)):
     url_links.append(urllib.request.urlopen((links[i])))
 
 
-# html_string=myurl.read()
 html_string_links=[]
 for i in range(len(links)):
     html_string_links.append(url_links[i].read())
@@ -49,18 +47,14 @@ for i in range(len(links)):
     text_links.append(bs4.BeautifulSoup(html_string_links[i],'html.parser').get_text())
 
 
-# text=text.encode("ascii","ignore")
 for i in range(len(links)):
     text_links[i]=text_links[i].encode("ascii","ignore")
 
 
-# text=text.decode()
 for i in range(len(links)):
     text_links[i]=text_links[i].decode()
 
-#
-# for i in modified_punctuation:
-#     text=text.replace(i," ")
+
 
 for i in range(len(links)):
     for j in modified_punctuation:
@@ -68,17 +62,9 @@ for i in range(len(links)):
 
 
 
-# text=text.split()
-
 for i in range(len(links)):
     text_links[i] = text_links[i].split()
 
-# print("Text: ",text)
-# TO to remove all special characters and left only ascii text
-# text_list=[]
-# for i in  text:
-#     if i not in string.punctuation:
-#         text_list.append(i)
 
 text_list_links=[]
 #  text_list_links is nested array
@@ -92,20 +78,11 @@ for i in range(len(links)):
 
 
 #TO lowercase all letters
-# for i in range(len(text_list)):
-#     text_list[i]=text_list[i].lower()
 
 for i in range(len(text_list_links)):
     for j in range(len(text_list_links[i])):
         text_list_links[i][j]=text_list_links[i][j].lower()
 
-#
-# for i in text_list:
-#     temp=""
-#     for j in i:
-#         if j in string.ascii_lowercase:
-#             temp=temp+j
-#     i=temp
 
 for i in range(len(text_list_links)):
     for j in text_list_links[i]:
@@ -117,173 +94,12 @@ for i in range(len(text_list_links)):
 
 #TO remove stop words
 #stopwords actually depend what u want, language keep changing.
-#there is short list of stopwords and long list of stopwords
-#different library different size of stopwords
+#different library different stopwords
 #download stopwords via nltk, go google
 from nltk.corpus import stopwords
 all_stopwords=stopwords.words('english')
 all_stopwords.append('has')
 print("Stopword:", all_stopwords)
-
-#you can jump to line 220 if you wish to use first stopwords from NLTK - shortcut ( CTRL +G )
-#this is default stopwords from the link provided
-# all_stopwords2=['a','about','above','after','again','against','all','am','an','and','any','are','aren\'t','as','at','be','because','been','before'
-# ,'being'
-# ,'below'
-# ,'between'
-# ,'both'
-# ,'but'
-# ,'by'
-# ,'can\'t'
-# ,'cannot'
-# ,'could'
-# ,'couldn\'t'
-# ,'did'
-# ,'didn\'t'
-# ,'do'
-# ,'does'
-# ,'doesn\'t'
-# ,'doing'
-# ,'don\'t'
-# ,'down'
-# ,'during'
-# ,'each'
-# ,'few'
-# ,'for'
-# ,'from'
-# ,'further'
-# ,'had'
-# ,'hadn\'t'
-# ,'has'
-# ,'hasn\'t'
-# ,'have'
-# ,'haven\'t'
-# ,'having'
-# ,'he'
-# ,'he\'d'
-# ,'he\'ll'
-# ,'he\'s'
-# ,'her'
-# ,'here'
-# ,'here\'s'
-# ,'hers'
-# ,'herself'
-# ,'him'
-# ,'himself'
-# ,'his'
-# ,'how'
-# ,'how\'s'
-# ,'i'
-# ,'i\'d'
-# ,'i\'ll'
-# ,'i\'m'
-# ,'i\'ve'
-# ,'if'
-# ,'in'
-# ,'into'
-# ,'is'
-# ,'isn\'t'
-# ,'it'
-# ,'it\'s'
-# ,'its'
-# ,'itself'
-# ,'let\'s'
-# ,'me'
-# ,'more'
-# ,'most'
-# ,'mustn\'t'
-# ,'my'
-# ,'myself'
-# ,'no'
-# ,'nor'
-# ,'not'
-# ,'of'
-# ,'off'
-# ,'on'
-# ,'once'
-# ,'only'
-# ,'or'
-# ,'other'
-# ,'ought'
-# ,'our'
-# ,'ours'
-# ,'ourselves'
-# ,'out'
-# ,'over'
-# ,'own'
-# ,'same'
-# ,'shan\'t'
-# ,'she'
-# ,'she\'d'
-# ,'she\'ll'
-# ,'she\'s'
-# ,'should'
-# ,'shouldn\'t'
-# ,'so'
-# ,'some'
-# ,'such'
-# ,'than'
-# ,'that'
-# ,'that\'s'
-# ,'the'
-# ,'their'
-# ,'theirs'
-# ,'them'
-# ,'themselves'
-# ,'then'
-# ,'there'
-# ,'there\'s'
-# ,'these'
-# ,'they'
-# ,'they\'d'
-# ,'they\'ll'
-# ,'they\'re'
-# ,'they\'ve'
-# ,'this'
-# ,'those'
-# ,'through'
-# ,'to'
-# ,'too'
-# ,'under'
-# ,'until'
-# ,'up'
-# ,'very'
-# ,'was'
-# ,'wasn\'t'
-# ,'we'
-# ,'we\'d'
-# ,'we\'ll'
-# ,'we\'re'
-# ,'we\'ve'
-# ,'were'
-# ,'weren\'t'
-# ,'what'
-# ,'what\'s'
-# ,'when'
-# ,'when\'s'
-# ,'where'
-# ,'where\'s'
-# ,'which'
-# ,'while'
-# ,'who'
-# ,'who\'s'
-# ,'whom'
-# ,'why'
-# ,'why\'s'
-# ,'with'
-# ,'won\'t'
-# ,'would'
-# ,'wouldn\'t'
-# ,'you'
-# ,'you\'d'
-# ,'you\'ll'
-# ,'you\'re'
-# ,'you\'ve'
-# ,'your'
-# ,'yours'
-# ,'yourself'
-# ,'yourselves'
-# ]
 
 
 #can actually done using shorter codes..... can actually do with python in or NLTK
@@ -293,11 +109,6 @@ print("Stopword:", all_stopwords)
 
 #to complete assignment, we use string matching algorithm, in fact binary search would be much faster T(n)= logn
 
-# newtext=" "
-# for i in text_list:
-#     newtext=newtext+i+" "
-# print("Text_List: ",text_list)
-# print("NEWTEST: ",newtext)
 
 newtext_links=[]
 for i in range (len(text_list_links)):
@@ -325,7 +136,6 @@ def boyer_moore(txt,pat,num):
     s=0
     while(s<=n-m):
         j=m-1
-        # print(j)
         front_space = False
         back_space = False
 
@@ -339,31 +149,17 @@ def boyer_moore(txt,pat,num):
 
         if txt[s+j]==" ":
             front_space=True
-        # print(front_space)
-        # print(back_space)
         if j < 0 and front_space==True and back_space==True:
-            # print("Pattern occur at shift = {}".format(s))
             txt2=re.sub(" "+pat+" "," ",txt2)
             num=num+1
             # s will shift to the next position where the pattern is matched
             s += (m - badChar[ord(txt[s + m])] if s + m < n else 1)
         else:
-            # print(ord(txt[s + j]))
             # s will shift to the next position by choosing 1 or the maximum shifting
             s += max(1, j - badChar[ord(txt[s + j])])
 
     return txt2,num
 
-
-# to compare each stopword to the text
-# demo_dictionary_stopwords={}
-# for i in all_stopwords:
-#     demo_num = 0
-#     newtext,num=boyer_moore(newtext,i,demo_num)
-#     if demo_num>0:
-#         demo_dictionary_stopwords[i]=demo_num
-#
-# print("Corrected text: ",newtext)
 
 dictionary_list_stopwords=[]
 for i in range(len(newtext_links)):
@@ -385,22 +181,12 @@ for i in range(len(dictionary_list_stopwords)):
         sum=sum+dictionary_list_stopwords[i][j]
     total_stopwords.append(sum)
 print("Sum: ",total_stopwords)
-# newtext_list=newtext.split()
-
-
 
 
 newtext_list_links=[]
 for i in range(len(newtext_links)):
     newtext_list_links.append(newtext_links[i].split())
 
-# dictt={}
-# for i in newtext_list:
-#     if i in dictt:
-#         dictt[i]=dictt[i]+1
-#     else:
-#         dictt[i]=1
-# print(dictt)
 
 dictt_links=[]
 for i in range(len(newtext_list_links)):
